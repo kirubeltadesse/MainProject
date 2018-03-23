dff62d5826e99f70c93f69c4bad4ab604b75510d
Kirubel Tadesse <kirubel.m.tadesse@gmail.com>
2018-03-23 18:15:17 -0400

    automatically feeding web_address

--- test.js
+++ test.js
@@ -1,101 +1,129 @@
 var WebPageTest = require('webpagetest')
 var fs = require('fs');
+
 const querystring = require('querystring');
-var wpt = new WebPageTest('https://www.webpagetest.org/','A.57c0ecce925470118d73687951af28ee')
+var wpt = new WebPageTest("https://www.webpagetest.org/","A.57c0ecce925470118d73687951af28ee")
+
+// collect all the web addresses
+w_addes = []
 
 // reading from a file
-var data = fs.readFileSync('univ_college.json')
-var Unv_web = JSON.parse(data);
+fs.readFile('univ_college.csv', 'utf8', function (err, data){
+	if (err){
+		throw err;
+		}
 
-//to see what is being read
-console.log(Unv_web);
+		// convert the data to string and remove end of line
+		var content = data.toString().split('\n');
+		for (var i in content){
+			var trimmed = content[i].trim();
+			if (trimmed.length !== 0){
+					w_addes.push(trimmed);
+				}
+			}
 
-/*
-// Creating a customMetrics
+		processweb_add();
+	});
 
-var customMetrics = [
-	'[iframes]',
-	'return document.getElementsByTagName("iframe").length',
-	'[ads],    							//looking for advertisment provider
-	'return Array.prototype.slice.call(document.getElementsByTagName("a")).filter(function (node) { return node.getAttribute("href").indexOf("ad.doubleclick.net") !== -1 }).length'
-	]
-*/
+function processweb_add() {
 
-/*
-wpt.runTest('https://css-tricks.com',{
-	connectivity: 'cable',                //specifing connection
-	location: 'Dulles:Chrome',			  // location	
-	firstViewOnly: false,
-	runs: 1,
-	pollResults: 5,
-	video: true
-}, function processTestResult(err, result){ 
-	//console.log(err || result) //this will return the specific testId etc...
-	console.log('Load time:', result.data.average.firstView.loadTime)
-	console.log('First byte:', result.data.average.firstView.TTFB)
-	console.log('Start render:', result.data.average.firstView.render)
-	console.log('Speed Index:', result.data.average.firstView.SpeedIndex)
-	console.log('DOM elements:', result.data.average.firstView.domElements)
-
+	// going throught a list of web address
+	for (var i = 0; i <w_addes.length - 70; i++) {
+		// console.log("this is :", i, w_addes[i]);
 	
-	console.log('(Doc complete) Requests:', result.data.average.firstView.requestsDoc)
-	console.log('(Doc complete) Byets in:', result.data.average.firstView.bytesInDoc)
 
 
-	console.log('(Fully loaded) Time:', result.data.average.firstView.fullyLoaded)
-	console.log('(Fully loaded) Requests:', result.data.average.firstView.requestsFull)
-	console.log('(Fully loaded) Bytes in:', result.data.average.firstView.bytesIn)
+		/*
+		// Creating a customMetrics
 
-	console.log('Waterfall view:', result.data.runs[1].firstView.images.waterfall)
-
-	var q_data = querystring.stringify({
-	'Load time:': result.data.average.firstView.loadTime,
-	'First byte:': result.data.average.firstView.TTFB,
-	'Start render:': result.data.average.firstView.render,
-	'Speed Index:': result.data.average.firstView.SpeedIndex,
-	'DOM elements:': result.data.average.firstView.domElements,
-	'(Doc complete) Requests:': result.data.average.firstView.requestsDoc,
-	'(Doc complete) Byets in:': result.data.average.firstView.bytesInDoc,
-	'(Fully loaded) Time:': result.data.average.firstView.fullyLoaded,
-	'(Fully loaded) Requests:': result.data.average.firstView.requestsFull,
-	'(Fully loaded) Bytes in:': result.data.average.firstView.bytesIn,
-	},';', ':');
+		var customMetrics = [
+			'[iframes]',
+			'return document.getElementsByTagName("iframe").length',
+			'[ads],    							//looking for advertisment provider
+			'return Array.prototype.slice.call(document.getElementsByTagName("a")).filter(function (node) { return node.getAttribute("href").indexOf("ad.doubleclick.net") !== -1 }).length'
+			]
+		*/
 
 
-	// custome Metrics values
-	//console.log('Iframes:', result.data.average.firstView.iframes)
-	//console.log('Ads:', result.data.average.firstView.ad)
-/*	bfinal = Buffer.concat([b1, b2, b3,b4,b5,b6,b7,b8,b9,b10]);
-	var json = bfinal.toJSON(bfinal);
-	console.log("The Buffer: ", bfinal)
-	console.log("The Json: ", json)
-*/
-	//var data = JSON.stringify(q_data, null, 2)
-/*
-	fs.writeFile('words2.json', q_data, response);
+		wpt.runTest(w_addes[i], {
+			connectivity: 'cable',                //specifing connection
+			location: 'Dulles:Chrome',			  // location	
+			firstViewOnly: false,
+			runs: 1,
+			pollResults: 5,
+			video: true
+		}, function processTestResult(err, result) { 
+			console.log(err || result)
+		/*
 
-	function response(err){
-		console.log('saving response');
+			//console.log(err || result) //this will return the specific testId etc...
+			console.log('Load time:', result.data.average.firstView.loadTime)
+			console.log('First byte:', result.data.average.firstView.TTFB)
+			console.log('Start render:', result.data.average.firstView.render)
+			console.log('Speed Index:', result.data.average.firstView.SpeedIndex)
+			console.log('DOM elements:', result.data.average.firstView.domElements)
+
+			
+			console.log('(Doc complete) Requests:', result.data.average.firstView.requestsDoc)
+			console.log('(Doc complete) Byets in:', result.data.average.firstView.bytesInDoc)
+
+
+			console.log('(Fully loaded) Time:', result.data.average.firstView.fullyLoaded)
+			console.log('(Fully loaded) Requests:', result.data.average.firstView.requestsFull)
+			console.log('(Fully loaded) Bytes in:', result.data.average.firstView.bytesIn)
+
+			console.log('Waterfall view:', result.data.runs[1].firstView.images.waterfall)
+			var q_data = querystring.stringify({
+			'Load time:': result.data.average.firstView.loadTime,
+			'First byte:': result.data.average.firstView.TTFB,
+			'Start render:': result.data.average.firstView.render,
+			'Speed Index:': result.data.average.firstView.SpeedIndex,
+			'DOM elements:': result.data.average.firstView.domElements,
+			'(Doc complete) Requests:': result.data.average.firstView.requestsDoc,
+			'(Doc complete) Byets in:': result.data.average.firstView.bytesInDoc,
+			'(Fully loaded) Time:': result.data.average.firstView.fullyLoaded,
+			'(Fully loaded) Requests:': result.data.average.firstView.requestsFull,
+			'(Fully loaded) Bytes in:': result.data.average.firstView.bytesIn,
+			},';', ':');
+
+
+			// custome Metrics values
+			//console.log('Iframes:', result.data.average.firstView.iframes)
+			//console.log('Ads:', result.data.average.firstView.ad)
+
+			//var data = JSON.stringify(q_data, null, 2)
+
+			fs.writeFile('words2.json', q_data, response);
+
+			function response(err){
+				console.log('saving response');
+			}
+		*/
+		})
 	}
+}
 
+// using the data.testId of the website this checks on the status of the test
+wpt.getTestStatus('180317_YS_ccecf2bcdde379e930fd2b0f3c6d625d', function processTestStatus(err, result) {
+	console.log(err || result)
 })
 
-wpt.getTestStatus('180317_YS_ccecf2bcdde379e930fd2b0f3c6d625d'
-, function processTestStatus(err, result){
+// using the data.testId of the website we get the test resutl
+wpt.getTestResults('180317_YS_ccecf2bcdde379e930fd2b0f3c6d625d',function processTestResult(err, result) {
 	console.log(err || result)
-
-	var data = JSON.stringify(result, null, 2)
-	fs.writeFile('words.json', data, finished);
+/*
+	var Get_data = JSON.stringify(result, null, 2)
+	fs.writeFile('words.json', Get_data, finished);
 
 	function finished(err){
 		console.log('File save');
-	}
+	}*/
+
 })
-*/
+
 /*****************************************
-Try to figure out how to feed multiple 
-web addresses to the script at once from a 
-text file instade of manually writing to the script
-and also the result to the json file with out deleting what already exists
+Try to figure out how to greb the data.testId
+for each web site that are being tested and 
+check the stages and the result on the fighly      
 *****************************************/
 
